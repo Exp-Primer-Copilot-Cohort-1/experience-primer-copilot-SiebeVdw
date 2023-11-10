fbf9a448d2038b8d9d6d113282a7a77d10fba659
SiebeVdw <143526762+SiebeVdw@users.noreply.github.com>
2023-11-10 20:35:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'Jhon',
+        age: 21,
+        skills: ['JavaScript', 'React', 'Redux', 'MongoDB', 'NodeJS', 'Python'],
+        salary: 1000
+    };
+    return member;
+}
